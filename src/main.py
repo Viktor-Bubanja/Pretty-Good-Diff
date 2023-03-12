192afeafa3918f748599a8b47d24311c9262e571
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2023-03-12 17:17:56 +0100

    Implement skeleton str diff checker + tests

--- src/main.py
+++ src/main.py
@@ -1,24 +1,13 @@
-import json
+from json import loads
+from json.decoder import JSONDecodeError
 from pydantic import BaseModel
 from typing import Optional
+from src.output import output
 
 
-def get_dict_diff(first_dict: dict, second_dict: dict, output_dict: Optional[dict] = None, sentinel = object()) -> dict:
-    if output_dict is None:
-        output_dict = {}
-
-    for key in set(first_dict.keys()) | set(second_dict.keys()):
-        first_value = first_dict.get(key, sentinel)
-        second_value = second_dict.get(key, sentinel)
-
-        if first_value != second_value:
-            if isinstance(first_value, dict) and isinstance(second_value, dict):
-                output_dict[key] = {}
-                output_dict[key] = get_dict_diff(first_value, second_value, output_dict[key], sentinel)
-            else:
-                output_dict[key] = (first_value, second_value)
-
-    return output_dict
+def main(first_object, second_object, sentinel = object()):
+    diff = get_diff(first_object, second_object, sentinel)
+    output(diff, sentinel)
 
 
 def get_diff(first_object, second_object, sentinel = object()):
@@ -30,8 +19,51 @@ def get_diff(first_object, second_object, sentinel = object()):
     elif isinstance(first_object, BaseModel):
         first_dict, second_dict = first_object.dict(), second_object.dict()
     elif input_type == str:  # TODO: check if input str is JSON, if not, compare strings
-        first_dict, second_dict = json.loads(first_object), json.loads(second_object)
+        try:
+            first_dict, second_dict = loads(first_object), loads(second_object)
+        except JSONDecodeError:
+            return _get_str_diff(first_object, second_object)
     else:
         raise NotImplementedError
 
-    return get_dict_diff(first_dict, second_dict, {}, sentinel)
+    return _get_dict_diff(first_dict, second_dict, {}, sentinel)
+
+
+def _get_dict_diff(first_dict: dict, second_dict: dict, output_dict: Optional[dict] = None, sentinel = object()) -> dict:
+    if output_dict is None:
+        output_dict = {}
+
+    for key in set(first_dict.keys()) | set(second_dict.keys()):
+        first_value = first_dict.get(key, sentinel)
+        second_value = second_dict.get(key, sentinel)
+
+        if first_value != second_value:
+            if isinstance(first_value, dict) and isinstance(second_value, dict):
+                output_dict[key] = {}
+                output_dict[key] = _get_dict_diff(first_value, second_value, output_dict[key], sentinel)
+            else:
+                output_dict[key] = (first_value, second_value)
+
+    return output_dict
+
+
+def _get_str_diff(first_str: str, second_str: str) -> str:
+    output = []
+    previously_matching = first_str[0] == second_str[0]
+    sub_strings = ["", ""]
+    for x, y in zip(first_str, second_str):
+        matching = x == y
+
+        if matching and previously_matching:
+            sub_strings[0] += x
+            sub_strings[1] += y
+        elif not matching and not previously_matching:
+            sub_strings[0] += x
+            sub_strings[1] += y
+        else:
+            output.append(sub_strings)
+            sub_strings = [x, y]
+
+        previously_matching = x == y
+    output.append(sub_strings)
+    return output
--- src/output.py
+++ src/output.py
@@ -1,27 +1,59 @@
-def output(output_dict: dict, sentinel = object()) -> None:
-    print(f"{_blue_output('actual')} =={_purple_output('expected')}\n\n")
+INDENT_SIZE = 4
 
+
+def output(diff, sentinel = object()):
+    print(f"\n{_blue_output('actual')} vs {_purple_output('expected')}\n\n")
+    if type(diff) == dict:
+        output_dict_diff(diff, sentinel)
+    if type(diff) == list:
+        output_str_diff(diff, sentinel)
+
+
+def output_dict_diff(output_dict: dict, sentinel = object(), indent = 0) -> None:
     missing_message = _red_output("<missing>")
 
-    for key, (actual, expected) in output_dict.items():
-        actual_output = missing_message if actual == sentinel else _blue_output(actual)
-        expected_output = missing_message if expected == sentinel else _purple_output(expected)
+    for key, value in output_dict.items():
+        if type(value) == dict:
+            print(indent * " " + _yellow_output(key))
+            output_dict_diff(value, sentinel, (indent + INDENT_SIZE))
+        else:
+            first_obj, second_obj = value
+            actual_output = missing_message if first_obj == sentinel else _blue_output(first_obj)
+            expected_output = missing_message if second_obj == sentinel else _purple_output(second_obj)
 
-        out_string = "{}:{},{}".format(_yellow_output(key), actual_output, expected_output)
-        print(out_string)
+            out_string = "{}: {} {}".format(_yellow_output(key), actual_output, expected_output)
+            print(indent * " " + out_string)
+
+
+def output_str_diff(substrings: list, sentinel = object()) -> None:
+    diff = ""
+    for first_sub, second_sub in substrings:
+        if first_sub == second_sub:
+            diff += _pale_green_output(first_sub)
+        else:
+            diff += f"{_blue_output(first_sub)}{_purple_output(second_sub)}"
+    print(diff)
 
 
 def _yellow_output(value):
-    return f"\033[93m {value}\033[00m"
+    return _colored_output(value, "93m")
 
 
 def _purple_output(value):
-    return f"\033[95m {value}\033[00m"
+    return _colored_output(value, "95m")
 
 
 def _blue_output(value):
-    return f"\033[96m {value}\033[00m"
+    return _colored_output(value, "96m")
 
 
 def _red_output(value):
-    return f"\033[31m {value}\033[00m"
+    return _colored_output(value, "31m")
+
+
+def _pale_green_output(value):
+    return _colored_output(value, "91m")
+
+
+def _colored_output(value, color_symbol):
+    return f"\033[{color_symbol}{value}\033[00m"
