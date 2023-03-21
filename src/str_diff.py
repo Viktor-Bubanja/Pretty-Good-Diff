5e1669748081d79480f03ac3ca8fca4d4d79bfa1
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2023-03-21 07:10:39 +0100

    Tidy up str diff function - extract some functions

--- src/str_diff.py
+++ src/str_diff.py
@@ -15,55 +15,29 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
             i_first += 1
             i_second += 1
             if i_first == len(first_str) and not first_str_done:
-                for existing_substring in output:
-                    if set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1]) and len(existing_substring[0]) < len(substrings[0]):
-                        output.remove(existing_substring)
-                add = True
-                for existing_substring in output:
-                    if (
-                            set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1])
-                        ) and len(existing_substring[0]) > len(substrings[0]):
-                        add = False
+                if worse_substring := worse_overlapping_substring(output, substrings):
+                    output.remove(worse_substring)
+                if not better_overlapping_substring(output, substrings):
+                    output.append(substrings)
                 i_first = 0
                 first_str_done = True
                 matching = False
-                if add:
-                    output.append(substrings)
                 substrings = ["", [[], []]]
             elif i_second == len(second_str) and not second_str_done:
-                for existing_substring in output:
-                    if (
-                            set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1])
-                        ) and len(existing_substring[0]) < len(substrings[0]):
-                        output.remove(existing_substring)
-                add = True
-                for existing_substring in output:
-                    if (set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1])
-                        ) or (existing_substring[1][0][0] < substrings[1][0][0] and existing_substring[1][1][0] > substrings[1][1][0]
-                        ) or (existing_substring[1][0][0] > substrings[1][0][0] and existing_substring[1][1][0] < substrings[1][1][0]
-                        ) and len(existing_substring[0]) > len(substrings[0]):
-                        add = False
+                if worse_substring := worse_overlapping_substring(output, substrings):
+                    output.remove(worse_substring)
+                if not better_overlapping_substring(output, substrings):
+                    output.append(substrings)
                 i_second = 0
                 second_str_done = True
                 matching = False
-                if add:
-                    output.append(substrings)
                 substrings = ["", [[], []]]
         elif x != y and previously_matching:
-            for existing_substring in output:
-                if (
-                        set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1])
-                    ) and len(existing_substring[0]) < len(substrings[0]):
-                    output.remove(existing_substring)
-            add = True
-            for existing_substring in output:
-                if (
-                        set(existing_substring[1][0]) & set(substrings[1][0]) or set(existing_substring[1][1]) & set(substrings[1][1])
-                    ) and len(existing_substring[0]) > len(substrings[0]):
-                    add = False
-            i_second = 0
-            if add:
+            if worse_substring := worse_overlapping_substring(output, substrings):
+                output.remove(worse_substring)
+            if not better_overlapping_substring(output, substrings):
                 output.append(substrings)
+            i_second = 0
             substrings = ["", [[], []]]
         elif x != y and not previously_matching:
             if i_second < len(second_str) - 1:
@@ -74,16 +48,42 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
             else:
                 break
         previously_matching = matching
-    add = True
-    for existing_substring in output:
-        if (
-                (existing_substring[1][0][-1] < i_first - 1 and existing_substring[1][1][0] > i_second - 1) or
-                (existing_substring[1][0][0] > i_first - 1 and existing_substring[1][1][-1] < i_second - 1)
-            ) and len(existing_substring[0]) < len(substrings[0]):
-            add = False
-    if add and substrings[0] and substrings not in output:
+
+    if substrings[0] and not better_overlapping_substring(output, substrings) and substrings not in output:
         output.append(substrings)
+
     print(output)
 
 
-get_diff("SAMATY", "SAMSAMANTH")
+def better_overlapping_substring(output, substring):
+    return overlapping_substring(output, substring, lambda x, y: x > y)
+
+
+def worse_overlapping_substring(output, substring):
+    return overlapping_substring(output, substring, lambda x, y: x < y)
+
+
+def overlapping_substring(output, substring, func):
+    sub, _ = substring
+    for existing_substring in output:
+        existing_sub, _ = existing_substring
+        if func(len(existing_sub), len(sub)) and substrings_overlap(existing_substring, substring):
+            return existing_substring
+
+    return None 
+
+
+def substrings_overlap(existing_substring, substring):
+    _, sub_mapping = substring
+    _, existing_mapping = existing_substring
+    if (set(existing_mapping[0]) & set(sub_mapping[0]) or set(existing_mapping[1]) & set(sub_mapping[1])
+    ) or (existing_mapping[0][0] < sub_mapping[0][0] and existing_mapping[1][0] > sub_mapping[1][0]
+    ) or (existing_mapping[0][0] > sub_mapping[0][0] and existing_mapping[1][0] < sub_mapping[1][0]
+    ):
+        return existing_substring
+            
+
+
+
+get_diff("SAMAT", "SAMSAMAN")
+# get_diff("12345", "12845")
