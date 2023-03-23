226dfc507694e29e40273186f0ce9d8911498b16
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2023-03-23 07:11:24 +0100

    Implement outputting string diff with updated str diff algorithm

--- src/output.py
+++ src/output.py
@@ -22,77 +22,41 @@ def output_dict_diff(output_dict: dict, indent = 0, matching_substrings = False)
                 output_str_diff(value)
             else:
                 first_obj, second_obj = value
-                actual_output = missing_message if first_obj is sentinel else _blue_output(first_obj)
-                expected_output = missing_message if second_obj is sentinel else _purple_output(second_obj)
+                actual_output = missing_message if first_obj is sentinel else _blue_background_output(first_obj)
+                expected_output = missing_message if second_obj is sentinel else _purple_background_output(second_obj)
 
                 out_string = "{}: {} {}".format(_yellow_output(key), actual_output, expected_output)
                 print(indent * " " + out_string)
 
 
-def output_dict_diff2(output_dict: dict, indent = 0) -> None:
-    missing_message = _red_output("<missing>")
-
-    for key, value in output_dict.items():
-        if type(value) == dict:
-            print(indent * " " + _yellow_output(key))
-            output_dict_diff2(value, (indent + INDENT_SIZE))
-        else:
-            output_str_diff(value)
-
 def output_str_diff(first_object, second_object, substrings: list) -> None:
-    breakpoint()
     diff = ""
-    mapped_indices_from_first_obj = set(chain(*[sub[1][0] for sub in substrings]))
+    first_obj_mappings, second_object_mappings = [sub[0] for sub in substrings], [sub[1] for sub in substrings]
 
-    for i, (x, y) in enumerate(zip_longest(first_object, second_object)):
-        if i in mapped_indices_from_first_obj:
-            diff += _pale_green_output(x)
-        else:
-            if x:
-                diff += _blue_output(x)
-            if y:
-                diff += _purple_output(y)
+    previous_first_index = -1
+    previous_second_index = -1
+    for x, y in zip(first_obj_mappings, second_object_mappings):
+        if x[0] > previous_first_index:
+            diff += _blue_background_output(first_object[slice(previous_first_index+1, x[0])])
+        if y[0] > previous_second_index:
+            diff += _purple_background_output(second_object[slice(previous_second_index+1, y[0])])
+        diff += first_object[slice(x[0], x[-1] + 1)]
+        previous_first_index = x[-1]
+        previous_second_index = y[-1]
 
     print(diff)
 
-
-def alternating_matching_and_unmatching_substrings(first_object, second_object, substrings: list) -> list:
-    mapped_indices_from_first_obj = [sub[1][0] for sub in substrings]
-    unmapped_indices_from_first_obj = []
-    previous_mapping = mapped_indices_from_first_obj[0]
-    for mapping in mapped_indices_from_first_obj:
-        start_index = previous_mapping[-1] + 1 if unmapped_indices_from_first_obj else 0
-        unmapped = list(range(start_index, mapping[0]))
-        previous_mapping = mapping
-        unmapped_indices_from_first_obj.append(unmapped)
-    if unmapped_indices_from_first_obj[-1][-1] < len(second_object):
-        unmapped_indices_from_first_obj.append(list(range(mapped_indices_from_first_obj[-1][-1] + 1, len(first_object))))
-    print(unmapped_indices_from_first_obj)
-
-    mapped_indices_from_second_obj = [sub[1][1] for sub in substrings]
-    unmapped_indices_from_second_obj = []
-    previous_mapping = mapped_indices_from_second_obj[0]
-    for mapping in mapped_indices_from_second_obj:
-        start_index = previous_mapping[-1] + 1 if unmapped_indices_from_second_obj else 0
-        unmapped = list(range(start_index, mapping[0]))
-        previous_mapping = mapping
-        unmapped_indices_from_second_obj.append(unmapped)
-    if unmapped_indices_from_second_obj[-1][-1] < len(second_object):
-        unmapped_indices_from_second_obj.append(list(range(mapped_indices_from_second_obj[-1][-1] + 1, len(second_object))))
-    print(unmapped_indices_from_second_obj)
-
-alternating_matching_and_unmatching_substrings("Y123345", "1288845W", [['12', [[1, 2], [0, 1]]], ['45', [[5, 6], [5, 6]]]])
 
 def _yellow_output(value):
     return _colored_output(value, "93m")
 
 
-def _purple_output(value):
-    return _colored_output(value, "95m")
+def _purple_background_output(value):
+    return _colored_output(value, "45m")
 
 
-def _blue_output(value):
-    return _colored_output(value, "96m")
+def _blue_background_output(value):
+    return _colored_output(value, "44m")
 
 
 def _red_output(value):
@@ -100,8 +64,10 @@ def _red_output(value):
 
 
 def _pale_green_output(value):
-    return _colored_output(value, "91m")
+    return _colored_output(value, "92m")
 
 
 def _colored_output(value, color_symbol):
     return f"\033[{color_symbol}{value}\033[00m"
+
+output_str_diff("Y123345", "1288845W", [[[1, 2], [0, 1]], [[5, 6], [5, 6]]])
--- src/str_diff.py
+++ src/str_diff.py
@@ -1,7 +1,7 @@
 def get_diff(first_str: str, second_str: str) -> list[str]:
     i_first, i_second = 0, 0
     output = []
-    substrings = ["", [[], []]]
+    mappings = [[], []]
     previously_matching = first_str[i_first] == second_str[i_second]
     first_str_done = second_str_done = False
 
@@ -9,37 +9,38 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
         x, y = first_str[i_first], second_str[i_second]
         matching = x == y
         if matching and previously_matching:
-            substrings[0] += x
-            substrings[1][0].append(i_first)
-            substrings[1][1].append(i_second)
+            mappings[0].append(i_first)
+            mappings[1].append(i_second)
             i_first += 1
             i_second += 1
             if i_first == len(first_str) and not first_str_done:
-                if worse_substring := worse_overlapping_substring(output, substrings):
+                if worse_substring := worse_overlapping_substring(output, mappings):
                     output.remove(worse_substring)
-                if not better_overlapping_substring(output, substrings):
-                    output.append(substrings)
+                if not better_overlapping_substring(output, mappings):
+                    output.append(mappings)
                 i_first = 0
                 first_str_done = True
                 matching = False
-                substrings = ["", [[], []]]
+                mappings = [[], []]
             elif i_second == len(second_str) and not second_str_done:
-                if worse_substring := worse_overlapping_substring(output, substrings):
+                if worse_substring := worse_overlapping_substring(output, mappings):
                     output.remove(worse_substring)
-                if not better_overlapping_substring(output, substrings):
-                    output.append(substrings)
+                if not better_overlapping_substring(output, mappings):
+                    output.append(mappings)
                 i_second = 0
                 second_str_done = True
                 matching = False
-                substrings = ["", [[], []]]
+                mappings = [[], []]
         elif x != y and previously_matching:
-            if worse_substring := worse_overlapping_substring(output, substrings):
+            if worse_substring := worse_overlapping_substring(output, mappings):
                 output.remove(worse_substring)
-            if not better_overlapping_substring(output, substrings):
-                output.append(substrings)
+            if not better_overlapping_substring(output, mappings):
+                output.append(mappings)
             i_second = 0
-            substrings = ["", [[], []]]
+            matching = False
+            mappings = [[], []]
         elif x != y and not previously_matching:
+            matching = False
             if i_second < len(second_str) - 1:
                 i_second += 1
             elif i_first < len(first_str) - 1:
@@ -49,14 +50,14 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
                 break
         previously_matching = matching
 
-    if substrings[0] and not better_overlapping_substring(output, substrings) and substrings not in output:
-        output.append(substrings)
+    if not better_overlapping_substring(output, mappings) and mappings not in output:
+        output.append(mappings)
 
-    print(output)
+    return output
 
 
 def better_overlapping_substring(output, substring):
-    return overlapping_substring(output, substring, lambda x, y: x > y)
+    return overlapping_substring(output, substring, lambda x, y: x >= y)
 
 
 def worse_overlapping_substring(output, substring):
@@ -67,23 +68,21 @@ def overlapping_substring(output, substring, func):
     sub, _ = substring
     for existing_substring in output:
         existing_sub, _ = existing_substring
-        if func(len(existing_sub), len(sub)) and substrings_overlap(existing_substring, substring):
+        if func(len(existing_sub), len(sub)) and mappings_overlap(existing_substring, substring):
             return existing_substring
 
     return None 
 
 
-def substrings_overlap(existing_substring, substring):
-    _, sub_mapping = substring
-    _, existing_mapping = existing_substring
-    if (set(existing_mapping[0]) & set(sub_mapping[0]) or set(existing_mapping[1]) & set(sub_mapping[1])
-    ) or (existing_mapping[0][0] < sub_mapping[0][0] and existing_mapping[1][0] > sub_mapping[1][0]
-    ) or (existing_mapping[0][0] > sub_mapping[0][0] and existing_mapping[1][0] < sub_mapping[1][0]
+def mappings_overlap(existing_mapping, new_mapping):
+    if not new_mapping[0] or not new_mapping[1]:
+        return False
+    if (set(existing_mapping[0]) & set(new_mapping[0]) or set(existing_mapping[1]) & set(new_mapping[1])
+    ) or (existing_mapping[0][0] < new_mapping[0][0] and existing_mapping[1][0] > new_mapping[1][0]
+    ) or (existing_mapping[0][0] > new_mapping[0][0] and existing_mapping[1][0] < new_mapping[1][0]
     ):
-        return existing_substring
-            
+        return True
 
 
-
-get_diff("SAMAT", "SAMSAMAN")
-# get_diff("12345", "12845")
+# get_diff("SAMAT", "SAMSAMAN")
+print(get_diff("Y1233345", "128845Z"))
