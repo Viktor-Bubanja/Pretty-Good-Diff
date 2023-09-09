bbb9ab770a4da00525f3f7552030c6210582b4f6
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2023-09-09 08:42:51 +0200

    Fix str diff algorithm
    There was previously a bug where the optimal solution would not be found based on the order of the given inputs. Now the algorithm is run with inputs with inputs first_str, second_str as well as second_str, first_str as a work-around of this bug. This adds unnecessary computation but is a simple and readable fix.

--- src/str_diff.py
+++ src/str_diff.py
@@ -1,7 +1,21 @@
 def get_diff(first_str: str, second_str: str) -> list[str]:
+    first_option = _calculate_diff(first_str, second_str)
+    second_option = _calculate_diff(second_str, first_str)
+
+    def len_matching_substrings(substrings):
+        return sum([len(sub[0]) for sub in substrings])
+
+    if len_matching_substrings(first_option) > len_matching_substrings(second_option):
+        return first_option
+    else:
+        second_option = [(sub[1], sub[0]) for sub in second_option]
+        return second_option
+
+
+def _calculate_diff(first_str: str, second_str: str) -> list[str]:
     i_first, i_second = 0, 0
     output = []
-    mappings = [[], []]
+    mappings = ([], [])
     previously_matching = first_str[i_first] == second_str[i_second]
     first_str_done = second_str_done = False
 
@@ -14,32 +28,32 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
             i_first += 1
             i_second += 1
             if i_first == len(first_str) and not first_str_done:
-                if worse_substring := worse_overlapping_substring(output, mappings):
+                if worse_substring := _worse_overlapping_substring(output, mappings):
                     output.remove(worse_substring)
-                if not better_overlapping_substring(output, mappings):
+                if not _better_overlapping_substring(output, mappings):
                     output.append(mappings)
                 i_first = 0
                 first_str_done = True
                 matching = False
-                mappings = [[], []]
+                mappings = ([], [])
             elif i_second == len(second_str) and not second_str_done:
-                if worse_substring := worse_overlapping_substring(output, mappings):
+                if worse_substring := _worse_overlapping_substring(output, mappings):
                     output.remove(worse_substring)
-                if not better_overlapping_substring(output, mappings):
+                if not _better_overlapping_substring(output, mappings):
                     output.append(mappings)
                 i_second = 0
                 second_str_done = True
                 matching = False
-                mappings = [[], []]
-        elif x != y and previously_matching:
-            if worse_substring := worse_overlapping_substring(output, mappings):
+                mappings = ([], [])
+        elif not matching and previously_matching:
+            if worse_substring := _worse_overlapping_substring(output, mappings):
                 output.remove(worse_substring)
-            if not better_overlapping_substring(output, mappings):
+            if not _better_overlapping_substring(output, mappings):
                 output.append(mappings)
             i_second = 0
             matching = False
-            mappings = [[], []]
-        elif x != y and not previously_matching:
+            mappings = ([], [])
+        elif not matching and not previously_matching:
             matching = False
             if i_second < len(second_str) - 1:
                 i_second += 1
@@ -50,39 +64,50 @@ def get_diff(first_str: str, second_str: str) -> list[str]:
                 break
         previously_matching = matching
 
-    if not better_overlapping_substring(output, mappings) and mappings not in output:
+    if not _better_overlapping_substring(output, mappings) and mappings not in output:
         output.append(mappings)
+
+    if output and output[-1] == ([], []):
+        output = output[:-1]
 
     return output
 
 
-def better_overlapping_substring(output, substring):
-    return overlapping_substring(output, substring, lambda x, y: x >= y)
+def _better_overlapping_substring(output, substring):
+    return _overlapping_substring(output, substring, lambda x, y: x >= y)
 
 
-def worse_overlapping_substring(output, substring):
-    return overlapping_substring(output, substring, lambda x, y: x < y)
+def _worse_overlapping_substring(output, substring):
+    return _overlapping_substring(output, substring, lambda x, y: x < y)
 
 
-def overlapping_substring(output, substring, func):
+def _overlapping_substring(output, substring, func):
     sub, _ = substring
     for existing_substring in output:
         existing_sub, _ = existing_substring
-        if func(len(existing_sub), len(sub)) and mappings_overlap(existing_substring, substring):
+        if func(len(existing_sub), len(sub)) and _mappings_overlap(
+            existing_substring, substring
+        ):
             return existing_substring
 
-    return None 
+    return None
 
 
-def mappings_overlap(existing_mapping, new_mapping):
+def _mappings_overlap(existing_mapping, new_mapping):
     if not new_mapping[0] or not new_mapping[1]:
         return False
-    if (set(existing_mapping[0]) & set(new_mapping[0]) or set(existing_mapping[1]) & set(new_mapping[1])
-    ) or (existing_mapping[0][0] < new_mapping[0][0] and existing_mapping[1][0] > new_mapping[1][0]
-    ) or (existing_mapping[0][0] > new_mapping[0][0] and existing_mapping[1][0] < new_mapping[1][0]
+    if (
+        (
+            set(existing_mapping[0]) & set(new_mapping[0])
+            or set(existing_mapping[1]) & set(new_mapping[1])
+        )
+        or (
+            existing_mapping[0][0] < new_mapping[0][0]
+            and existing_mapping[1][0] > new_mapping[1][0]
+        )
+        or (
+            existing_mapping[0][0] > new_mapping[0][0]
+            and existing_mapping[1][0] < new_mapping[1][0]
+        )
     ):
         return True
-
-
-# get_diff("SAMAT", "SAMSAMAN")
-print(get_diff("Y1233345", "128845Z"))
